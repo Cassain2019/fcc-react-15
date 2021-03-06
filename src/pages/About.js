7822ee83723d2fa16f0b60ed270ff442ac57d51a
cassain2019 <cashif.hussain@gmail.com>
2021-03-06 16:04:18 +0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        We are continuously making partners and strengthen himself to providing “One Window Corporate Solutions” 
+        to our customers, 
+        we deals in all sort of national and international brand having products related to office supplies.
+      </p>
+    </section>
+  )
+}
+
+export default About
